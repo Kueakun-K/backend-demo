63e521895b4598be4720afc5bed1de72e9d3d690
Black Kitten <blackkitten010@gmail.com>
2021-04-17 18:44:14 +0700

    first commit

---
+++ controllers/deleteTaskController.js
@@ -0,0 +1,7 @@
+
+const {deleteTaskservice, deleteTaskService} = require('../services')
+
+module.exports = async function deleteTaskController(req, res){
+    deleteTaskService(req.params.id)
+    return res.send("Deleted")
+}
