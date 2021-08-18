14f48bc4576e0719ca3e803182aed3219b2a963f
Black Kitten <blackkitten010@gmail.com>
2021-08-18 22:56:21 +0700

    registerrrr

--- services/index.js
+++ services/index.js
@@ -2,10 +2,12 @@ const getTasksService = require('./getTasksService')
 const createTaskService = require('./createTaskService')
 const updateTaskService = require('./updateTaskService')
 const deleteTaskService = require('./deleteTaskService')
+const registerService = require('./registerService')
 
 module.exports = {
     getTasksService,
     createTaskService,
     updateTaskService,
     deleteTaskService,
+    registerService,
 }
