b817226c136e44d3d53f7560a0d9c8424e97d6dd
Black Kitten <blackkitten010@gmail.com>
2021-08-18 22:22:38 +0700

    regis

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,13 @@
 const getTasksController = require("./getTasksController");
 const createTaskController = require("./createTaskController");
 const updateTaskController = require("./updateTaskController");
-const deleteTaskController = require('./deleteTaskController')
+const deleteTaskController = require('./deleteTaskController');
+const registerController = require('./registerController')
 
 module.exports = {
     getTasksController,
     createTaskController,
     updateTaskController,
     deleteTaskController,
+    registerController,
 };
