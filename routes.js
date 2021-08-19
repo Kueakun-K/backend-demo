b7e5c3c121d45de3e8f5aa76573b867a495a6474
Black Kitten <blackkitten010@gmail.com>
2021-08-19 12:57:31 +0700

    check error

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const controllers = require("./controllers");
+const {check,validationResult} = require('express-validator');
 
 router.get("/tasks", (req, res) => controllers.getTasksController(req, res));
 
@@ -10,6 +11,23 @@ router.put("/task", (req, res) => controllers.updateTaskController(req, res));
 
 router.delete("/task/:id", (req, res) => controllers.deleteTaskController(req, res));
 
-router.post("/user", (req, res) => controllers.registerController(req, res));
+router.post("/user", [
+    check('User', 'name is required').not().isEmpty(),
+    check('Password', 'password is required').not().isEmpty(),
+],(req, res) => controllers.registerController(req, res));
+
+
+const result = validationResult(req);
+var errors = result.errors;
+
+for (var key in errors) {
+    console.log(errors[key].value);
+}
+
+if(!result.isEmpty()) {
+    res.render('register',{
+        errors: errors
+    })
+}
 
 module.exports = router
