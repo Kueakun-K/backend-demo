a08f9aeefdd819c6060dbed012e556d4fc15acc3
Black Kitten <blackkitten010@gmail.com>
2021-08-20 12:06:13 +0700

    login

--- models/RegisterModel.js
+++ models/RegisterModel.js
@@ -1,8 +1,18 @@
 const mongoose = require('mongoose')
 
 const RegisterSchema = mongoose.Schema({
-    User: String,
-    Password: String,
+    username:{
+        type:String,
+        require:true
+    },
+    password:{
+        type:String,
+        require:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
 })
 
 const RegisterModel = mongoose.model('User', RegisterSchema)
--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controllers = require("./controllers");
 const {RegisterModel} = require("./models");
-// const {check,validationResult} = require('express-validator');
 
 router.get("/tasks", (req, res) => controllers.getTasksController(req, res));
 
@@ -12,37 +11,21 @@ router.put("/task", (req, res) => controllers.updateTaskController(req, res));
 
 router.delete("/task/:id", (req, res) => controllers.deleteTaskController(req, res));
 
-router.post("/user", (req, res) => controllers.registerController(req, res));
+router.post("/signup", (req, res) => controllers.registerController(req, res));
 
-router.post('/login', async (req, res) => {
-    const { User, Password } = req.body
-    const user = await RegisterModel.findOne({
-      User,
-      Password
-    })
-    if (user) {
-      console.log('login_complete')
-    } else {
-      console.log('login_fail')
-    }
+router.post('/login',  (req, res) => {
+    const { username, password } = req.body
+    RegisterModel.findOne({ username: username}, (err, RegisterModel) => {
+      if(RegisterModel){
+          if(password === RegisterModel.password ) {
+              res.send({message: "Login Successfull", RegisterModel: RegisterModel})
+          } else {
+              res.send({ message: "Password didn't match"})
+          }
+      } else {
+          res.send({message: "User not registered"})
+      }
   })
+}) 
 
 module.exports = router
-// [
-    // check('User', 'name is required').not().isEmpty(),
-    // check('Password', 'password is required').not().isEmpty(),
-// ],
-
-
-// const result = validationResult(req);
-// var errors = result.errors;
-
-// for (var key in errors) {
-//     console.log(errors[key].value);
-// }
-
-// if(!result.isEmpty()) {
-//     res.render('register',{
-//         errors: errors
-//     })
-// }
