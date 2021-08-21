1cdc405568e8996886f37f8e2a6797196b9f3142
Black Kitten <blackkitten010@gmail.com>
2021-08-21 12:27:02 +0700

    bcrypt

--- services/registerService.js
+++ services/registerService.js
@@ -1,7 +1,10 @@
 const {RegisterModel} = require('../models')
+const bcrypt = require('bcryptjs') 
 
 module.exports = async function registerService(data){
     const user = new RegisterModel(data)
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(password, salt)
     return user.save()
 
 }
