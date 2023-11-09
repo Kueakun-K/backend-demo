2176e0d9aa862f129f4359b3e8a46877c391fc20
Kueakun-K <blackkitten010@gmail.com>
2023-11-10 00:57:55 +0700

    change database

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-mongoose.connect('mongodb+srv://Kueakun:Aa_1250101508286@cluster0.htr4xsn.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true}).then(()=>{
+mongoose.connect('mongodb+srv://63010095:63010095@cluster0.yahbnwe.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true}).then(()=>{
     console.log('Database connected')
 })
 
